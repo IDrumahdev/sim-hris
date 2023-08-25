699f66192dac9d3020415895d19ef74d3a28608b
IDrumahdev <ibnudirsan@gmail.com>
2023-08-25 14:06:01 +0700

    composer update

--- database/migrations/2023_08_25_112037_create_employees_table.php
+++ database/migrations/2023_08_25_112037_create_employees_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->string('nip');
             $table->string('full_name');
             $table->date('birth_day');
             $table->string('gender');
