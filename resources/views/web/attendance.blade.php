e866afab1bd1c07f380890c822c77ea1197eaaf3
IDrumahdev <ibnudirsan@gmail.com>
2023-08-27 19:02:30 +0700

    System Attendance Employee

--- resources/views/web/attendance.blade.php
+++ resources/views/web/attendance.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
-        {{ env('APP_NAME') }} | Attendance Employee
+        System Attendance Employee
     </title>
     
     <!-- Fonts -->
